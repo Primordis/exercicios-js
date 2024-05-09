e052a0f28a301bc3b444e4ceaa4650f27b7e4fda
JOAO PINHEIRO <42075957835@info.local>
2024-05-09 09:52:06 -0300

    feat: Exercício 8 feito.

--- ex8.js
+++ ex8.js
@@ -10,5 +10,25 @@
 // IMC de 35.0 a 39.9: Obesidade grau 2
 // IMC maior ou igual a 40.0: Obesidade grau 3
 
-// IMC de 35.0 a 39.9: Obesidade grau 2
-// IMC maior ou igual a 40.0: Obesidade grau 3
+let peso = parseInt(prompt("Digite seu peso em quilogramas."))
+let altura = parseFloat(prompt("Digite sua altura em metros."))
+let imc = (peso / (altura * altura))
+
+if(imc < 18.5){
+    console.log("Você está abaixo do peso. Coma mais, e de maneira saudável, junto de fazer exercícios.")
+}
+else if(imc <= 24.9){
+    console.log("Parabéns! Você está em um peso normal, nem abaixo nem acima do comum.")
+}
+else if(imc <= 29.9){
+    console.log("Você está acima do peso. Mude sua dieta para comidas que goste porém são saudáveis e tenha uma mente forte para conseguir se tornar seu eu ideal.")
+}
+else if(imc <= 34.9){
+    console.log("Você tem obesidade de grau 1. É recomendado assistência médica para que você possa voltar a um peso normal.")
+}
+else if(imc <= 39.9){
+    console.log("Você tem obesidade de grau 2. É recomendado assistência médica para que você possa voltar a um peso normal. ")
+}
+else if(imc >= 40.0){
+    console.log("Você tem obesidade de grau 3. É recomendado assistência médica para que você possa voltar a um peso normal.")
+}
