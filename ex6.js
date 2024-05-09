5041c704dc4f10f0a5badf09b08bd90dd99cad9c
JOAO PINHEIRO <42075957835@info.local>
2024-05-09 09:21:57 -0300

    feat: exercício 6 feito

--- ex6.js
+++ ex6.js
@@ -1,3 +1,11 @@
 // Exercício 6: Verificação de idade
 // Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
 // de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+let idade = parseInt(prompt("Digite sua idade."))
+
+if (idade >= 18){
+    console.log("Você é maior de idade e pode acessar todas as partes deste site.")
+} else{
+    console.log("Você é menor de idade e terá parte do conteúdo deste site reduzido.")
+}
