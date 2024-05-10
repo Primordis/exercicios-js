9b8f1baef81d466fa889839e1670f86e86a226b5
JOAO PINHEIRO <42075957835@info.local>
2024-05-10 10:51:46 -0300

    feat: cálculo de idade dentro do exercício 6 e também um console log que mostra ao usuário sua idade.

--- ex6.js
+++ ex6.js
@@ -2,10 +2,13 @@
 // Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
 // de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
 
-let idade = parseInt(prompt("Digite sua idade."))
+let anonascimento = parseInt(prompt("Digite seu ano de nascimento"))
+let anoatual = new Date().getFullYear()
+let idade = anoatual - anonascimento
 
 if (idade >= 18){
     console.log("Você é maior de idade e pode acessar todas as partes deste site.")
+    console.log("Sua idade é de " + idade + ".")
 } else{
     console.log("Você é menor de idade e terá parte do conteúdo deste site reduzido.")
 }
