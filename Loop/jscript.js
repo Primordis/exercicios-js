c0af2044c1bbb4be5813fa287b55ac323aaae7a4
JOAO PINHEIRO <42075957835@info.local>
2024-05-13 10:44:12 -0300

    feat: ex 1

---
+++ Loop/jscript.js
@@ -0,0 +1,4 @@
+for (let contador = 1; contador <= 10; contador ++){
+    console.log(contador)
+}
+// contador ++ soma do 1 até o valor máximo. -- subtrai até o valor máximo. Caso não for dado nenhum, será infinito. For é (Algo | Condição | Expressão Final)
---
+++ Loop/loop.js
@@ -0,0 +1,40 @@
+// // for(let num = 0; num <= 10; num ++){
+// //     console.log(num)
+// // }
+// for(let cont = 10; cont >= 0; cont--){
+//     console.log(cont)
+// }
+
+// let money = 0
+
+// while (money < 100){
+//     // money = money + 10 (O de baixo faz o mesmo.)
+//     money += 10
+//     console.log("Seu dinheiro é " + money)
+// }
+
+// Do While - condicional. Do decide o que acontecerá, e while limita.
+
+// let money = 0
+
+// do {
+//     console.log("your money is " + money)
+//     money += 100
+
+// } while (money < 5000)
+
+// Array é listinha. Usa colchete. exemplo: Let x = ["Coisa", "Coisa"]
+
+// let frutas = ["Banana", "Laranja", "Maça", "Pitaya", "Melancia"]
+
+// // console.log (frutas[3])
+
+// // frutas.forEach(frutas => {
+// //     console.log("A fruta é " + frutas)
+// // })
+
+// for (let indice = 0; indice < frutas.length; indice++){
+//     console.log(frutas[indice])
+// } if(frutas[indice] == "Pitaya"){
+//     console.log("Eu gosto de " + frutas[indice])
+// }
