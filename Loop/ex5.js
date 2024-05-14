b32d8c56e5a0e24320082c9440b5e23878b9215d
JOAO PINHEIRO <42075957835@info.local>
2024-05-14 08:48:05 -0300

    feat: Exercício 5 feito.

---
+++ Loop/ex5.js
@@ -0,0 +1,19 @@
+// let n1 = 0, n2 = 1, nextTerm
+
+// for (let fibo = 1; fibo <= 10; fibo ++){
+//     console.log(n1)
+//     nextTerm = n1 + n2
+//     n1 = n2
+//     n2 = nextTerm
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let n1 = 1, n2 = 1, nextTerm
+
+for (let fibo = 1; fibo <= 10; fibo ++){
+    console.log(n1)
+    nextTerm = n1 + n2
+    n1 = n2
+    n2 = nextTerm
+}
