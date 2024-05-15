48ce430ab33e8f832cf8c22b84d1ed27c687f2fa
JOAO PINHEIRO <42075957835@info.local>
2024-05-15 10:55:49 -0300

    feat: Exemplos de função

---
+++ Function/functions.js
@@ -0,0 +1,32 @@
+// // function saudacao() {
+
+// //     let nome = prompt("qual seu nome")
+
+// //     let horaatual = new Date().getHours()
+
+// //     if (horaatual > 6 && horaatual < 12) {
+// //         window.alert("bom dia " + nome)
+// //     }
+// //     else if (horaatual >= 12 && horaatual < 18) {
+// //         window.alert("boa tarde " + nome)
+
+// //     } else { window.alert("boa noite " + nome) }
+// // }
+// // saudacao()
+// // saudacao()
+
+// function soma(num1, num2, num3){
+//     let soma = num1 + num2 + num3
+//     return(soma)
+// }
+// let resultado = soma (5, 6, 9)
+// console.log("resultado é " + resultado )
+
+let professor = {}
+
+    professor.nome = prompt("su nombre?")
+    professor.sobrenome = prompt("su apellido?")
+    professor.idade = prompt("su idade?")
+
+    console.log(professor)
+
