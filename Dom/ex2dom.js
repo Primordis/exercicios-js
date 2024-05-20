4e6222e0283ff7156993c15f3e96472af1dc3760
JOAO PINHEIRO <42075957835@info.local>
2024-05-20 10:59:39 -0300

    feat: Progresso em exercício 2

--- Dom/ex2dom.js
+++ Dom/ex2dom.js
@@ -1,1 +1,17 @@
 // adicionar e remover itens de uma lista
+
+
+
+const addItem = () => {
+    let novoItem = document.createElement("li")
+    novoItem.textContent = "Novo Item"
+    let lista = document.getElementById("minhaListaex2")
+    lista.appendChild(novoItem);
+}
+
+const removerItem = () => {
+    let novoItem = document.eraseElement("li")
+    let lista = document.getElementById("minhaListaex2")
+    minhaListaex2.innerhtml = ""
+    lista.appendChild(novoItem);
+}
