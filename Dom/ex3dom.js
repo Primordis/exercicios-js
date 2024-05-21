2c7311ecf57d2bef604c72996142a2a532d02b1d
JOAO PINHEIRO <42075957835@info.local>
2024-05-21 09:27:48 -0300

    feat: Progresso em Exercício 3

--- Dom/ex3dom.js
+++ Dom/ex3dom.js
@@ -1,1 +1,13 @@
 // fechar a página, voltar e mostrar sua localização.
+
+const voltarPagina = () => {
+    window.history.back()
+}
+
+const fecharPagina = () => {
+    window.close()
+}
+
+const rastrearCoords = () => {
+    navigator.geolocation.getCurrentPosition()
+}
