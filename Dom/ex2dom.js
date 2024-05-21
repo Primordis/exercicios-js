beda23a055841562ffc898b27901d7f82689a451
JOAO PINHEIRO <42075957835@info.local>
2024-05-21 08:20:17 -0300

    feat: Exercício 2 feito.

--- Dom/ex2dom.js
+++ Dom/ex2dom.js
@@ -1,17 +1,16 @@
 // adicionar e remover itens de uma lista
 
 
+let novoItem = document.createElement("li")
+let lista = document.getElementById("minhaListaex2")
 
 const addItem = () => {
     let novoItem = document.createElement("li")
-    novoItem.textContent = "Novo Item"
     let lista = document.getElementById("minhaListaex2")
+    novoItem.textContent = "Novo Item"
     lista.appendChild(novoItem);
 }
 
-const removerItem = () => {
-    let novoItem = document.eraseElement("li")
-    let lista = document.getElementById("minhaListaex2")
-    minhaListaex2.innerhtml = ""
-    lista.appendChild(novoItem);
-}
+function clearAll() {while (lista.firstChild) {
+        lista.removeChild(lista.firstChild)
+    }}
